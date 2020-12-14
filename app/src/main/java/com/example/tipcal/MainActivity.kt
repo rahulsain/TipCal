d3ec8f72fe291dd7ea328eeaaa72588ed09e7dc2
rahulsain3000 <50570677+rahulsain@users.noreply.github.com>
2020-12-14 14:33:50 +0530

    completed the app with bugs fixed

--- app/src/main/java/com/example/tipcal/MainActivity.kt
+++ app/src/main/java/com/example/tipcal/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tipcal
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tipcal.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
     }
 
 }
