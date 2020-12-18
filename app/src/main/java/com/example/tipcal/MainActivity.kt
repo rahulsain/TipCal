47ad9667596fbf06c9494e89e2980b4f4263303d
rahulsain3000 <50570677+rahulsain@users.noreply.github.com>
2020-12-18 17:40:18 +0530

    updated edit text and switch to MDC

--- app/src/main/java/com/example/tipcal/MainActivity.kt
+++ app/src/main/java/com/example/tipcal/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         // If the cost is null or 0, then display 0 tip and exit this function early.
         if (cost == null || cost == 0.0) {
